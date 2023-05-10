d25630b7d9eb807bf657f95edab51359364eedae
Ivan Kolominskiy <ifkolominskiy@edu.hse.ru>
2023-05-10 19:48:05 +0300

    UserSchema has been updated

--- validations/registerValidation.js
+++ validations/registerValidation.js
@@ -2,5 +2,6 @@ import {body} from "express-validator";
 
 export const registerValidation = [
     body('login').isLength({min: 3}),
-    body('password').isLength({min: 8})
+    body('email').isEmail(),
+    body('password').isLength({min: 8}),
 ]

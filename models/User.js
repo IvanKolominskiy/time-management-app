6fff78e1e8cb322ac73a8625d354ead3b90b42dc
Ivan Kolominskiy <ifkolominskiy@edu.hse.ru>
2023-05-10 17:03:53 +0300

    UserSchema has been added

---
+++ models/User.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+
+const UserSchema = new mongoose.Schema({
+    login: {
+        type: String,
+        required: true
+    },
+    passwordHash: {
+        type: String,
+        required: true
+    }
+}, {
+    timestamps: true
+})
+
+export default mongoose.model('User', UserSchema);

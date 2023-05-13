89aba8eb7a32241c6473e40d3cee5baa3ccd96d3
Ivan Kolominskiy <ifkolominskiy@edu.hse.ru>
2023-05-13 18:30:37 +0300

    adding of tasks has been fixed

--- routes/dashboardRoute.js
+++ routes/dashboardRoute.js
@@ -3,6 +3,7 @@ import {validationResult} from "express-validator";
 import {taskValidation} from "../validations/taskValidation.js";
 import TaskModel from "../models/Task.js";
 import path from 'path';
+import checkAuth from "../utils/checkAuth.js";
 
 export const dashboardRoute = new Router();
 const __dirname = path.resolve();
@@ -12,7 +13,7 @@ dashboardRoute.get('/dashboard', (req, res) => {
     res.sendFile(path.join(__dirname, 'views', 'dashboard.html'));
 });
 
-dashboardRoute.post('/dashboard', urlencodedParser, taskValidation, async (req, res) => {
+dashboardRoute.post('/dashboard', checkAuth, urlencodedParser, taskValidation, async (req, res) => {
     try {
         const errors = validationResult(req);
         if (!errors.isEmpty()) {

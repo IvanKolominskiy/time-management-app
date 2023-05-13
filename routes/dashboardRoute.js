ada668e8de967ffdf0129dc7562e7132b01aebc3
Ivan Kolominskiy <ifkolominskiy@edu.hse.ru>
2023-05-13 13:06:56 +0300

    dashboardRoute has been fixed

--- routes/dashboardRoute.js
+++ routes/dashboardRoute.js
@@ -1,10 +1,9 @@
 import {Router} from "express";
 import path from 'path';
-import {loginRoute} from "./loginRoute.js";
 
 export const dashboardRoute = new Router();
 const __dirname = path.resolve();
 
-loginRoute.get('/dashboard', (req, res) => {
+dashboardRoute.get('/dashboard', (req, res) => {
     res.sendFile(path.join(__dirname, 'views', 'dashboard.html'));
 });
